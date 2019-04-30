11916fb75e343a3552d77936a1e2abddd361d6d6
Arai Shinya <>
2019-04-30 18:16:12 +0900

    firebaseデプロイまでいけた、次はfirebaseのCRUDを作る

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCd0EMyALTnqKXSSo-WSETgw-I6aOhk7wI',
+    authDomain: 'angular-todo-433ac.firebaseapp.com',
+    databaseURL: 'https://angular-todo-433ac.firebaseio.com',
+    projectId: 'angular-todo-433ac',
+    storageBucket: 'angular-todo-433ac.appspot.com',
+    messagingSenderId: '481204850937'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCd0EMyALTnqKXSSo-WSETgw-I6aOhk7wI',
+    authDomain: 'angular-todo-433ac.firebaseapp.com',
+    databaseURL: 'https://angular-todo-433ac.firebaseio.com',
+    projectId: 'angular-todo-433ac',
+    storageBucket: 'angular-todo-433ac.appspot.com',
+    messagingSenderId: '481204850937'
+  }
 };
 
 /*
